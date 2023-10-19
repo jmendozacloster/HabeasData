77ebfea1bc3b9b6f6996c4d7cb9e5f1ebe1cf6dd
jmendozacloster <jmendoza@closterpharma.com>
2023-10-19 14:40:00 -0500

    iconos index v2

--- php_index/index.php
+++ php_index/index.php
@@ -7,7 +7,9 @@
     <title>Habeas Data | Closter Pharma</title>
 
     <link rel="stylesheet" href="../Sytle/estilos.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+
 
 </head>
 </head>
@@ -63,9 +65,9 @@
                     <td><?php echo $d->origen_cedula; ?></td>
             <!-- Enlace para ver y descargar el PDF -->
             <td>
-                <a href="../PDFS/<?php echo $d->name; ?>.pdf" target="_blank"><i class="fas fa-eye"></i>❌ Ver PDF</a>
-                <a href="../PDFS/<?php echo $d->name; ?>.pdf" target="_blank" download><i class="fas fa-download"></i>🥱 Descargar PDF</a>
-                <a href="../php/eliminar.php?id=<?php echo $d->id; ?>"><i class="fas fa-trash-alt"></i>😎 Eliminar</a>
+                <a href="../PDFS/<?php echo $d->name; ?>.pdf" target="_blank"><i class="fas fa-eye"></i> Ver PDF</a>
+                <a href="../PDFS/<?php echo $d->name; ?>.pdf" target="_blank" download><i class="fas fa-download"></i> Descargar PDF</a>
+                <a href="../php/eliminar.php?id=<?php echo $d->id; ?>"><i class="fas fa-trash-alt"></i> Eliminar</a>
             </td>
                 </tr>
             <?php endforeach; ?>
