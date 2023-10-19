e87ac066db272658ed8dc4c1ccda63c7b84ecca5
LauTriana26 <ltriana@closterpharma.com>
2023-10-19 16:02:41 -0500

    Cambio Nuevo Prueba10

--- php_index/index.php
+++ php_index/index.php
@@ -49,6 +49,7 @@
 
     <?php if (count($data)) : ?>
         <!-- Tabla para mostrar los documentos firmados -->
+        <table border="1">
             <thead>
                 <!-- Columnas de la tabla -->
                 <!-- <th>#</th> -->
